fc86e8b1f780a010643fc0c7156847add10db771
Semyon1104 <sema_titov_04@mail.ru>
2024-11-30 19:03:15 +0300

    add 6 card on page

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+function createCard(info) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+  
+    cardImage.src = info.link;
+    cardImage.alt = info.name;
+    cardTitle.textContent = info.name;
+  
+    return cardElement;
+  }
 
 // @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(info => {
+  const card = createCard(info);
+  placesList.append(card);
+});
+
